f6343c2dfec728271591f769b7420dd7eaf944c1
מתן סימן טוב <matan_siman_tov@MacBook-Air-sl-mtn.local>
2024-11-14 15:01:15 +0200

    d

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,28 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args [0];
+                int n = Integer.parseInt(args[1]);
+                System.out.println(word);
+                int length = word.length();
+                String check = "AEFHILMNORSX";
+                for (int i = 0; i < length; i++) {
+                        String pre;
+                        char c = word.charAt(i);
+                        if (check.indexOf(c)>= 0) {
+                                pre = "an";      
+                        }
+                        else {
+                                pre = "a";
+                        }
+                        System.out.println("Give me " + pre + " " + c + ": " + c + "!");
+                }
+              System.out.println("What does that spell?");
+              for (int i = 0; i < n; i++) {
+                        System.out.println(word + "!!!");
+              }
+
+
+        
         }
 }
